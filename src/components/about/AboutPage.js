9ac16c5adfbb68b8ba4af6eee17f17b661943a64
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-26 23:31:44 -0800

    updated to add basic react components

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div className = "jumbotron">
+                <h1>About Get Happy</h1>
+                <p>A sign-up flow to onboard potential customers</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div className = "jumbotron">
+                <h1>Get Happy</h1>
+                <p>A sign-up flow to onboard potential customers</p>
+                <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
