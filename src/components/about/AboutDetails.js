86fb949f193790dfc9547691d35d9526deb1525f
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-27 22:15:38 -0800

    updated details on about-details page

--- src/components/about/AboutDetails.js
+++ src/components/about/AboutDetails.js
@@ -4,8 +4,13 @@ class AboutDetails extends React.Component {
     render() {
         return (
             <div className = "about-details">
-                <h1 className="about-header">About Get Happy</h1>
-                <h2 className="h2-about">A sign-up flow to onboard potential customers</h2>
+                <h1 className="about-header">Details</h1>
+                    <div className="details-div">
+                        <p className="p-about"><span className="span-title">Audience:</span> Young adults who enjoy traveling and going on road trips</p>
+                        <p className="p-about"><span className="span-title">Age Range:</span> Between 20 to 35</p>
+                        <p className="p-about"><span className="span-title">Personality:</span> They are adventurous, open to new experiences, willing to make decisions on the fly, and are open minded</p>
+                        <p className="p-about"><span className="span-title">Hobbies:</span> Hiking, camping, going to the beach, paddle boarding, trying new food, enjoying time with friends, photography, art, and spontaneous activities</p>
+                    </div>
             </div>
             
         );
