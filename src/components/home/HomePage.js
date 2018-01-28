ad42c38cdc26ff125e6295e036c7f30f8423b73e
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-27 23:08:09 -0800

    updated css on home and about page, added background to jumbotron

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -8,7 +8,7 @@ class HomePage extends React.Component {
                 <h1 className="home-header">Get Happy</h1>
                 <h2 className="h2-header">A demo sign-up flow for <span className="hm">Happy Money</span></h2  >
                 <Link to="about" className="btn btn-custom btn-lg">Learn More</Link>
-                <Link to="signup" className="btn btn-custom btn-lg">SignUp</Link>
+                <Link to="signup" className="btn btn-custom btn-lg">Sign Up</Link>
             </div>
         );
     }
