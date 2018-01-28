fa4290499cbd5a70dc507b9c1d476340090e1148
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-27 19:01:42 -0800

    updated to include sign up page and routing

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -8,6 +8,7 @@ class HomePage extends React.Component {
                 <h1>Get Happy</h1>
                 <p>A sign-up flow to onboard potential customers</p>
                 <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
+                <Link to="signup" className="btn btn-success btn-lg">SignUp</Link>
             </div>
         );
     }
---
+++ src/components/signup/SignUpPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SignUpPage extends React.Component {
+    render() {
+        return (
+            <div className = "jumbotron">
+                <h1>Sign Up!</h1>
+                <p>A sign-up flow to onboard potential customers</p>
+            </div>
+        );
+    }
+}
+
+export default SignUpPage;
