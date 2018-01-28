3753587154c142709d88d1de291fc52ff3d51bdb
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-28 11:31:36 -0800

    updated question 1 to correct from email validation

--- src/components/signup/questions/Q1Name.js
+++ src/components/signup/questions/Q1Name.js
@@ -7,7 +7,7 @@ class Q1Name extends React.Component {
                 <h1 id="question-h1">What is your name?</h1>
                 <form>
                     <div className="form-group">
-                        <input type="email" className="form-control email-form" id="yourNameHere" aria-describedby="name" placeholder="" />
+                        <input type="name" className="form-control text-form custom-form" id="yourNameHere" aria-describedby="name" placeholder="" />
                     </div>
                     <button type="submit" className="btn btn-custom btn-lg">Next Question!</button>
                 </form>
