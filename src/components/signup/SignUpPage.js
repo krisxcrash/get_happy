224372ba532079956757352ba81ab8e4d0a1032d
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-27 22:38:06 -0800

    updated signup and css

--- src/components/signup/SignUpPage.js
+++ src/components/signup/SignUpPage.js
@@ -4,12 +4,14 @@ class SignUpPage extends React.Component {
     render() {
         return (
             <div className = "container-fluid">
+            <div className="question-box">
                 <h1 id="question-1">What is your email?</h1>
                 <form>
                     <div className="form-group">
                         <input type="email" className="form-control email-form" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="example@gmail.com" />
                     </div>
                 </form>
+            </div>
             </div>
         );
     }
