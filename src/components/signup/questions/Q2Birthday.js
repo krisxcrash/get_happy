d81b0b3c0b37f35cf14cc7d26a737eef85815ad1
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-28 11:10:09 -0800

    updated to add questions and formpage

--- src/components/signup/questions/Q2Birthday.js
+++ src/components/signup/questions/Q2Birthday.js
@@ -4,10 +4,10 @@ class Q2Birthday extends React.Component {
     render() {
         return (
             <div className="question-box">
-                <h1 id="question-1">What is your email?</h1>
+                <h1 id="question-h1">When is your birthday?</h1>
                 <form>
                     <div className="form-group">
-                        <input type="email" className="form-control email-form" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="example@gmail.com" />
+                        <input type="date" className="form-control email-form" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="example@gmail.com" />
                     </div>
                 </form>
             </div>
--- src/components/signup/questions/Q3City.js
+++ src/components/signup/questions/Q3City.js
@@ -4,7 +4,7 @@ class Q3City extends React.Component {
     render() {
         return (
             <div className="question-box">
-                <h1 id="question-1">What is your email?</h1>
+                <h1 id="question-h1">What is your email?</h1>
                 <form>
                     <div className="form-group">
                         <input type="email" className="form-control email-form" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="example@gmail.com" />
