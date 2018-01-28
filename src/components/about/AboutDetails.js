e01d2c5a0699c229dd027bb8eba3adae2eedb76f
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-27 21:35:41 -0800

    updated CSS on about page

--- src/components/about/AboutDetails.js
+++ src/components/about/AboutDetails.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-class AboutPage extends React.Component {
+class AboutDetails extends React.Component {
     render() {
         return (
-            <div className = "about-jumbo">
+            <div className = "about-details">
                 <h1 className="about-header">About Get Happy</h1>
                 <h2 className="h2-about">A sign-up flow to onboard potential customers</h2>
-                <span className="arrow">&#x25BF;</span>
             </div>
             
         );
     }
 }
 
-export default AboutPage;
+export default AboutDetails;
