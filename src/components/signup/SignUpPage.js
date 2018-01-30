69af2fd9decfbe8bb82d56dbfd10be480b083865
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-29 17:42:32 -0800

    updated function on signup page

--- src/components/signup/SignUpPage.js
+++ src/components/signup/SignUpPage.js
@@ -10,16 +10,15 @@ import Q7Email from './questions/Q7Email';
 class SignUpPage extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            currentSlide: 0
-        };
     }
-    nextSlide() {
-        this.setState({
-            currentSlide: this.state.currentSlide + 1
-        });
-    }
+    //     this.state = {
+    //         currentSlide: 0
+    //     };
+    // }
+    // nextSlide() {
+    //     this.setState({
+    //         currentSlide: this.state.currentSlide + 1
+    //     });
     render() {
         const components = [Q1Name, Q2Birthday, Q3City, Q4YouReady, Q5Setting, Q6Length, Q7Email];
         const componentsToRender = components.map((Component, i) => (
@@ -29,15 +28,23 @@ class SignUpPage extends React.Component {
         let i=0;
 
         function handleClick() {
+            // event.preventDefault();
             i++;
-            return componentsToRender[i];
+            if (i <= components.length ) {
+                return componentsToRender[i];
+            }
+            else {
+                return `<div className="questions">
+                            <h1 id="question-h1">Thanks! You will receive an email with your Adventure Map soon!</h1>
+                        </div>`;
+            }
         }
 
         return (
             <div className = "container-fluid signup-page">
                 <div className = "question-box">
-                    {componentsToRender[i]}
-                    <button type="submit" className="btn btn-custom btn-lg" onClick={handleClick => { i++; console.log(i); return componentsToRender[i];}}>Next Question!</button>
+                    {componentsToRender[0]}
+                    <button type="submit" className="btn btn-custom btn-lg" onClick={handleClick()}>Next Question!</button>
                 </div>
             </div>
         );
