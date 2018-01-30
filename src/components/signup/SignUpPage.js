a2ed8a4566a325bee12c7a02af2a7270382929bf
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-29 18:14:27 -0800

    updated function onClick - now pulls in each component to console..just need to append to the div

--- src/components/signup/SignUpPage.js
+++ src/components/signup/SignUpPage.js
@@ -11,14 +11,7 @@ class SignUpPage extends React.Component {
     constructor(props) {
         super(props);
     }
-    //     this.state = {
-    //         currentSlide: 0
-    //     };
-    // }
-    // nextSlide() {
-    //     this.setState({
-    //         currentSlide: this.state.currentSlide + 1
-    //     });
+
     render() {
         const components = [Q1Name, Q2Birthday, Q3City, Q4YouReady, Q5Setting, Q6Length, Q7Email];
         const componentsToRender = components.map((Component, i) => (
@@ -27,24 +20,11 @@ class SignUpPage extends React.Component {
 
         let i=0;
 
-        function handleClick() {
-            // event.preventDefault();
-            i++;
-            if (i <= components.length ) {
-                return componentsToRender[i];
-            }
-            else {
-                return `<div className="questions">
-                            <h1 id="question-h1">Thanks! You will receive an email with your Adventure Map soon!</h1>
-                        </div>`;
-            }
-        }
-
         return (
             <div className = "container-fluid signup-page">
                 <div className = "question-box">
-                    {componentsToRender[0]}
-                    <button type="submit" className="btn btn-custom btn-lg" onClick={handleClick()}>Next Question!</button>
+                    {componentsToRender[i]}
+                    <button type="submit" className="btn btn-custom btn-lg" onClick={handleClick => { i++; if (i < components.length) {console.log(i);  console.log(componentsToRender[i]);}}}>Next Question!</button>
                 </div>
             </div>
         );
