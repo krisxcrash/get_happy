e270b3815e60fc36f04bdbcefe9ea5821c5531aa
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-30 21:11:03 -0800

    updated to add homepage and about page animations

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -7,8 +7,8 @@ class HomePage extends React.Component {
             <div className= "home-jumbo">
                 <h1 className="home-header">Adventure Map</h1>
                 <h2 className="h2-header">A demo sign-up flow for <span className="hm">Happy Money</span></h2  >
-                <Link to="about" className="btn btn-custom btn-lg">Learn More</Link>
-                <Link to="signup" className="btn btn-custom btn-lg">Sign Up</Link>
+                <Link to="about" className="btn btn-custom btn-home btn-lg">Learn More</Link>
+                <Link to="signup" className="btn btn-custom btn-home btn-lg">Sign Up</Link>
             </div>
         );
     }
