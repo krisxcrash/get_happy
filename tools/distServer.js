80909fd1e21cdd816a463efe95a5150be8a21790
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-31 20:38:34 -0800

    updated server - testing to see if affects deploy on heroku

--- tools/distServer.js
+++ tools/distServer.js
@@ -7,17 +7,21 @@ import open from 'open';
 const port = 3000;
 const app = express();
 
-app.use(express.static('dist'));
+// app.use(express.static('dist'));
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(port, function(err) {
-    if (err) {
-        console.log(err);
-    } 
-    else {
-        open(`http://localhost:${port}`);
-    }
-});
+app.use(express.static(__dirname + '/'));
+
+app.listen(process.env.PORT || 8080);
+
+// app.listen(port, function(err) {
+//     if (err) {
+//         console.log(err);
+//     } 
+//     else {
+//         open(`http://localhost:${port}`);
+//     }
+// });
