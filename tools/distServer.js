4ff174daf4d9e8c703c81091bcf113b2f0b269ca
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-31 22:09:06 -0800

    updated distServer

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,30 +1,25 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-import favicon from 'serve-favicon';
 
 /* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
 
-// app.use(express.static('dist'));
+app.use(express.static('dist'));
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.use(express.static(__dirname + '/'));
-
-app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')))
-
 app.listen(process.env.PORT || 8080);
 
-// app.listen(port, function(err) {
-//     if (err) {
-//         console.log(err);
-//     } 
-//     else {
-//         open(`http://localhost:${port}`);
-//     }
-// });
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    } 
+    else {
+        open(`http://localhost:${port}`);
+    }
+});
