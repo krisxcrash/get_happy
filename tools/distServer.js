36a46b294cf4c26a41cbeb8b94dc97af52e00e93
Kristine Martin <kristinenicolemartin@gmail.com>
2018-02-01 20:56:45 -0800

    updated to change port from 8080 to 5000

--- tools/distServer.js
+++ tools/distServer.js
@@ -13,7 +13,7 @@ app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
 
 // app.listen(port, function(err) {
 //     if (err) {
